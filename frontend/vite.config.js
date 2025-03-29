52e2090fb53a90912cdbc042157c7920db4cda9c
charan <sravanamcharan20@gmail.com>
2025-03-29 20:46:47 +0530

    added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   server: {
     proxy: {
       '/api': {
         target: 'https://sharables-production.up.railway.app',
         changeOrigin: true,
-        secure: false
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
       }
     }
   }
